b535088fbd0393fcc91cc8134dc8a86b5d1bfc3f
이민호 <minho.lee0716@gmail.com>
2021-12-03 10:18:54 +0900

    Add: alembic init

---
+++ alembic/versions/101e6e0ff3b8_first_test_init.py
@@ -0,0 +1,24 @@
+"""First test init
+
+Revision ID: 101e6e0ff3b8
+Revises: 
+Create Date: 2021-12-02 18:15:21.174117
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '101e6e0ff3b8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
